81e5095be8c317593f48b6fc9aa39c5858adfbab
Estes <Russell.Estes@cmc.com>
2020-01-24 16:22:08 -0500

    Add project files.

---
+++ Giraffe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Giraffe
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Cint num = Convert.ToInt32("51");
+			Console.WriteLine(num + 6);
+
+		}
+	}
+}
